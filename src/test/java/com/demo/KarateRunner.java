46395666e37a2a8c9a00dda7d28cf1a693b43b7d
Geetu <japendareddy@gmail.com>
2026-05-07 15:41:18 -0400

    fix: Add Allure JUnit5 extension to KarateRunner for report generation

--- src/test/java/com/demo/KarateRunner.java
+++ src/test/java/com/demo/KarateRunner.java
@@ -1,10 +1,17 @@
 package com.demo;
+
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import io.qameta.allure.junit5.AllureJunit5;
+
+@ExtendWith(AllureJunit5.class)
 class KarateRunner {
+
     @Karate.Test
     Karate testPosCashTransaction() {
         return Karate.run("classpath:karate/pos/pos-cash-transaction.feature");
     }
+
     @Karate.Test
     Karate testPosCardPayment() {
         return Karate.run("classpath:karate/pos/pos-card-payment.feature");
